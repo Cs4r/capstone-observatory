c8b56a394737e842bb970f01612c6c2e9f56ae76
César Aguilera <cesar.aguilera.p@gmail.com>
2017-04-08 20:26:43 +0100

    first milestone

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -2,6 +2,8 @@ package observatory
 
 import java.time.LocalDate
 
+import scala.io.Source
+
 /**
   * 1st milestone: data extraction
   */
@@ -17,7 +19,35 @@ object Extraction {
     // ignore data coming from stations that have no GPS coordinates
     // The temperature field contains a decimal value (or 9999.9 if missing)
 
-    ???
+    val stationStream = getClass.getResourceAsStream(stationsFile)
+    val stationToLocation = Source.fromInputStream(stationStream).getLines()
+      .map(line => line.split(","))
+      .filter(chunks => chunks.size >= 4)
+      .map(chunks => {
+        val location = Location(chunks(2).toDouble, chunks(3).toDouble)
+        (Station(chunks(0), chunks(1)), location)
+      })
+      .toMap
+
+    val temperaturesStream = getClass.getResourceAsStream(temperaturesFile)
+
+    Source.fromInputStream(temperaturesStream).getLines()
+      .map(line => line.split(","))
+      .map(chunks => {
+        val station = Station(chunks(0), chunks(1))
+        val month = chunks(2).toInt
+        val day = chunks(3).toInt
+        val temperatureInFahrenheit = if (chunks(4).isEmpty) 9999.9 else chunks(4).toDouble
+        val temperature = toCelsius(temperatureInFahrenheit)
+
+        val date = LocalDate.of(year, month, day)
+        val location = stationToLocation.get(station)
+        
+        if (location.isDefined) Some((date, location.get, temperature)) else None
+      })
+      .filter(_.isDefined)
+      .map(_.get)
+      .toIterable
   }
 
   /**
@@ -25,7 +55,13 @@ object Extraction {
     * @return A sequence containing, for each location, the average temperature over the year.
     */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Double)]): Iterable[(Location, Double)] = {
-    ???
+    records
+      .groupBy(_._2)
+      .mapValues(triplet => triplet.map(_._3).sum / triplet.size)
+
   }
+
+
+  private def toCelsius(inFahrenheit: Double) = (inFahrenheit - 32) * 5 / 9
 
 }
--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -7,5 +7,10 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ExtractionTest extends FunSuite {
 
-  
+
+  test("locateTemperatures returns non-emtpy iterable for stations.csv and 1975.csv") {
+    assert(Extraction.locateTemperatures(1975, "/stations.csv", "/1975.csv").isEmpty == false)
+  }
+
+
 }
