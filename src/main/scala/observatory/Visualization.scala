2475f4d47907f3de40da165012587d4d7bdfe532
César Aguilera <cesar.aguilera.p@gmail.com>
2017-04-10 23:57:25 +0100

    implemented method to interpolate color

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -9,7 +9,7 @@ object Visualization {
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
+    * @param location     Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
@@ -18,16 +18,59 @@ object Visualization {
 
   /**
     * @param points Pairs containing a value and its associated color
-    * @param value The value to interpolate
+    * @param value  The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    ???
+    val asSeq = points.toIndexedSeq.sortBy(_._1)
+    if (asSeq.isEmpty) {
+      throw new IllegalArgumentException
+    }
+
+    var left = 0
+    for (i <- 1 until asSeq.size) {
+      if (asSeq(i)._1 < value) {
+        left = i
+      }
+    }
+
+    var right = asSeq.size - 1
+
+    for (i <- asSeq.size - 2 to 0 by -1) {
+      if (asSeq(i)._1 > value) {
+        right = i
+      }
+    }
+
+    if (left + 1 == right - 1 && asSeq(left + 1)._1 == value) {
+      return asSeq(left + 1)._2
+    } else if (left == 0 && value <= asSeq(left)._1) {
+      asSeq(left)._2
+    } else if (right == asSeq.size - 1 && value >= asSeq(right)._1) {
+      asSeq(right)._2
+    } else {
+      interpolateColors(asSeq(left), asSeq(right), value)
+    }
+
+
   }
+
+  private def interpolateColors(a: (Double, Color), b: (Double, Color), x: Double): Color = {
+    val red = a._2.red + (x - a._1) * (b._2.red - a._2.red) / (b._1 - a._1)
+
+    val green = a._2.green + (x - a._1) * (b._2.green - a._2.green) / (b._1 - a._1)
+
+    val blue = a._2.blue + (x - a._1) * (b._2.blue - a._2.blue) / (b._1 - a._1)
+
+    Color(round(red), round(green), round(blue))
+  }
+
+  def round(d: Double) = math.round(d).toInt
+
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
+    * @param colors       Color scale
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)]): Image = {
--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -1,6 +1,7 @@
 package observatory
 
 
+import observatory.Visualization.interpolateColor
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -10,4 +11,92 @@ import org.scalatest.prop.Checkers
 class VisualizationTest extends FunSuite with Checkers {
 
 
+  val white = Color(255, 255, 255)
+  val black = Color(0, 0, 0)
+  val red = Color(255, 0, 0)
+  val green = Color(0, 255, 0)
+
+  val sortedPoints: Iterable[(Double, Color)] = List((-60.0, black), (0.0, red), (5.0, green), (60.0, white))
+  val unsortedPoints: Iterable[(Double, Color)] = List((60.0, white), (-60.0, black), (0.0, red))
+
+
+  test("interpolateColor should return color for lowest temperature when temperature is lower than the lowest") {
+
+    val color = interpolateColor(sortedPoints, -160.0)
+
+    assert(color == black)
+  }
+
+  test("interpolateColor should return color for highest temperature when temperature is higher than the highest") {
+
+
+    val color = interpolateColor(sortedPoints, 160.0)
+
+    assert(color == white)
+  }
+
+
+  test("interpolateColor should return color for highest temperature when temperature is higher than the highest and the iterable is not sorted") {
+
+
+    val color: Color = interpolateColor(unsortedPoints, 160)
+
+    assert(color == white)
+  }
+
+  test("interpolateColor should return color for lowest temperature when temperature is lower than the lowest and the iterable is not sorted") {
+    val color: Color = interpolateColor(unsortedPoints, -160)
+
+    assert(color == black)
+  }
+
+  test("interpolateColor should return same color as reference point when temperature is equal to the temperature of one of the reference points") {
+    val color = interpolateColor(sortedPoints, 5.0)
+
+    assert(color == green)
+  }
+
+  test("interpolateColor should return interpolated color when temperature is not in the list of reference points") {
+    val color = interpolateColor(sortedPoints, 2.5)
+
+    assert(color == Color(128, 128, 0))
+  }
+
+
+  test("interpolateColor should return interpolated color when temperature is not in the list of reference points and there are more than 3 colors") {
+    val points: Iterable[(Double, Color)] = List((60.0, white), (-60.0, black), (0.0, red), (4.0, green))
+    val color = interpolateColor(points, 2)
+
+    assert(color == Color(128, 128, 0))
+  }
+
+  test("interpolateColor should return first color when there is only one reference color") {
+    val color = interpolateColor(List((60.0, white)), 2)
+    assert(color == white)
+  }
+
+  test("interpolateColor should throw IllegalArgument if iterable is empty") {
+
+    intercept[IllegalArgumentException] {
+      interpolateColor(List(), 1)
+    }
+  }
+
+  test("color interpolation") {
+    val color = interpolateColor(List((-1.0, Color(255, 0, 0)), (0.0, Color(0, 0, 255))), -0.75)
+
+    val expected = Color(191, 0, 64)
+
+    assert(color == expected)
+  }
+
+  test("exceeding the greatest value of a color scale should return the color associated with the greatest value") {
+
+    val color = interpolateColor(List((-85.46584346884845, Color(255, 0, 0)), (-77.57141733273376, Color(0, 0, 255))), -77.57141733273376)
+
+    val expected = Color(0, 0, 255)
+
+    assert(color == expected)
+
+  }
 }
